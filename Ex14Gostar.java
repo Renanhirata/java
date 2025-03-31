fb1aa694d62b02330a52c744fd1030845ce0bf48
Renanhirata <renanhiramatsua1a@gmail.com>
2025-03-31 15:44:25 -0300

    Ex14Gostar.java

---
+++ Ex14Gostar.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+ Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+ 
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex14Gostar
+{
+    public static void main(String[] args) {
+    Scanner ler = new Scanner (System.in);
+    int l;
+     
+     for(int i=1; i <=10  ; i++)
+    System.out.println("Eu gosto de estudar Algoritmos!");
+    }
+}
