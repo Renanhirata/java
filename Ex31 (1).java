aeb616ef62f2af9fff5a982252fc0f4c49db679a
Renanhirata <renanhiramatsu1a@gmail.com>
2025-04-09 07:35:18 -0300

    Ex31 (1).java

---
+++ Ex31 (1).java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+ Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+ Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex31
+{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+       int t = 1;
+  
+        while (t <= 19) {
+            t++;
+            System.out.println(t); }
+        
+        
+    }
+}
