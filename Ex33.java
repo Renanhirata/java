0c9bf5ec068ef0ae286de008d803bca291ec1799
Renanhirata <renanhiramatsu1a@gmail.com>
2025-04-09 07:39:05 -0300

    Ex33.java

---
+++ Ex33.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Faça um programa que leia 10 números e informe a soma e a média dos números.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Ex33 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int contador = 1;
+        double numero, soma = 0, media;
+
+        while (contador <= 10) {
+            System.out.print("Digite o " + contador + "º número: ");
+            numero = ler.nextDouble();
+            soma += numero;
+            contador++;
+        }
+
+        media = soma / 10;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
+    }
+}
