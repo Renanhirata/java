b86fcd6649f2b8154b3f59ad53991f48b13d5a16
Renanhirata <renanhiramatsu1a@gmail.com>
2025-04-14 15:27:36 -0300

    ExemploArray01.java

---
+++ ExemploArray01.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Exemplo: Utilizando Array e visualizando a saida de dados
+
+*******************************************************************************/
+public class ExemploArray01
+{
+	public static void main(String[] args) {
+		int[] numeros = new int [5];
+		
+		numeros[0] = 10;
+		numeros[1] = 20;
+		numeros[2] = 30;
+		numeros[3] = 40;
+		numeros[4] = 50;
+		
+		for(int i=0; i <numeros.length; i++){
+		    System.out.println("O" + (i+1) + "° número é:" + numeros[i]);
+		}
+		}
+}
