f9d5bb4d96f2021ce369fac889011ddfd1f48fd4
Renanhirata <renanhiramatsu1a@gmail.com>
2025-04-30 11:10:53 -0300

    Ex001.java

---
+++ Ex001.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Exercicio 001
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex001
+{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int numero;
+		
+		System.out.println("Insira o número: ");
+		numero = ler.nextInt();
+		
+		System.out.println("O número informado foi [" +numero+ "]");
+	}
+}
