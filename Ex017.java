9bb01ee4bf8d4f1f67a9e21a14e618d9f12736d1
Renanhirata <renanhiramatsu1a@gmail.com>
2025-05-12 07:39:01 -0300

    Ex017.java

---
+++ Ex017.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Exercicio 017
+Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+*******************************************************************************/
+public class Ex017
+{
+	public static void main(String[] args) {
+		System.out.println("Os números ímpares entre 1 e 50: ");
+		for (int i = 1; i <= 50; i++) {
+		if (i % 2 != 0){
+		    System.out.println(i);
+		}
+	
+	}
+} 
+}
